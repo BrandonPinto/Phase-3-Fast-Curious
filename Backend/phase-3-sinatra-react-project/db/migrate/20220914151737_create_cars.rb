aa145fc32084c308e4eca3019fb4f3c7a3ad14f6
Brandon <b.pintony@gmail.com>
2022-09-14 15:06:54 -0400

    dsfd

--- Backend/phase-3-sinatra-react-project/db/migrate/20220914151737_create_cars.rb
+++ Backend/phase-3-sinatra-react-project/db/migrate/20220914151737_create_cars.rb
@@ -1,12 +1,8 @@
 class CreateCars < ActiveRecord::Migration[6.1]
   def change
     create_table :cars do |t|
-      t.string :engine_name
       t.string :car_part
-      t.string :wheel_name
-      t.string :rim_name
-      t.string :spoiler_name
-      t.string :body_name
+      t.string :carpart_name
       t.integer :price
       t.string :imgURL
     end
